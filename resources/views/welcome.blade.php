386a9ee539f782a65b10f997836375c3ee3e8c80
tenshicakes <jmontalan8@gmail.com>
2026-09-17 22:44:57 +0800

    GREEEEN GIANT!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,10 @@
         <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
         <p class="text-gray-600 mt-2">Wow its working...great</p>
         <p>LET IT GREEN LET IT GREEN!</p>
+        <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
+        <p class="text-gray-600 mt-2">Wow its working...great</p>
+        <p>LET IT GREEN LET IT GREEN!</p>
+        <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
+        
     </div>
 </x-layouts.app>
