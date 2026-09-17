4417d20836fcdf7c13502d3ea37fbeeb9bebe6a1
tenshicakes <jmontalan8@gmail.com>
2026-09-17 22:44:07 +0800

    LET GREEN LIGHT SHUN OPEN THE COMMIT BOX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
         <p class="text-gray-600 mt-2">Wow its working...great</p>
-        <p>LET THE COMMITS TURN LIGHT GREEN!!!</p>
+        <p>LET IT GREEN LET IT GREEN!</p>
     </div>
 </x-layouts.app>
