8c0e1b7e1533a0e71c1953e1dc19b58333816427
tenshicakes <jmontalan8@gmail.com>
2026-09-17 22:41:34 +0800

    added some texts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
-        <p class="text-gray-600 mt-2">Tailwind CSS and app.blade.php Layout are working!</p>
+        <p class="text-gray-600 mt-2">Wow its working...great</p>
     </div>
 </x-layouts.app>
