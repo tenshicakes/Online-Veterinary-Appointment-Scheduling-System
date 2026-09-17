076fdc7699ccad6d3f38a57b91df64a8bc5316b9
tenshicakes <jmontalan8@gmail.com>
2026-09-17 22:43:05 +0800

    LET IT GREEEEN!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <h1 class="text-4xl font-bold text-blue-600">Doc Jay's Vet Clinic</h1>
         <p class="text-gray-600 mt-2">Wow its working...great</p>
-        <p>for commits purposes only</p>
+        <p>LET THE COMMITS TURN LIGHT GREEN!!!</p>
     </div>
 </x-layouts.app>
